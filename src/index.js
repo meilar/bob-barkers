725b0966752b5d3f91660d4d46bd95eca01b9930
Josh Tillinghast <jwtill@icloud.com>
2022-02-14 16:08:21 -0800

    Add styling to justify center cards

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import testCode from './test-code.js';
+// import testCode from './test-code.js';
 
-$(document).ready(function() {
-  let msg = testCode();
-  console.log("This page is loading correctly " + msg);
-});
+// $(document).ready(function() {
+//   let msg = testCode();
+//   console.log("This page is loading correctly " + msg);
+// });

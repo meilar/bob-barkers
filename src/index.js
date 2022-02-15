84e8c094e29ecfd9c8e6aefe05e92247c62c38c6
Liam Eller <liamthelastson@gmail.com>
2022-02-15 11:09:43 -0800

    Add click functions for game logic, save api call to global variable and change displayProduct() function slightly.
    Co-Authored-By: MIkah Mathews <mikah@outofband.org>

--- src/index.js
+++ src/index.js
@@ -1,31 +1,51 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import AmazonService from './js/amazonService.js';
+import Player from './js/gameLogic.js';
+let player1 = new Player(3, 0); 
+
 
 function displayErrors(error) {
   $("#errors").html(error);
 }
 
-function displayProduct(response) {
-  // format the response in some way
-  let i = Math.floor(Math.random() * 50);
-  $(".title").html(response.bestsellers[i].title);
-  $(".image").html(`<img src='${response.bestsellers[i].image}' alt='${response.bestsellers[0].title}'>`);
-  $(".price").html(`$${response.bestsellers[i].price.value}`);
+function displayProduct(productArray, i) {
+  $(".title").html(productArray[i].title);
+  $(".image").html(`<img src='${productArray[i].image}' alt='${productArray[i].title}'>`);
+  $(".price").html(`$${productArray[i].price.value}`);
 }
 
-$(document).ready(function() {
+function displayPrice(userGuess) {
+  player1.guessCheck(userGuess);
+  $("#results-screen").show(); 
+  // this is for showing the price of the product and and users points gained from guess on a results screen
+}
+
+$(document).ready(function() { 
+  let productArray;
+  let i = 0;
   $("#start-game").on('click', function() {
     let searchCategory = //Add in Category selection buttons with endpoint values
     AmazonService.makeAPICall(searchCategory).then(function(response) {
       if (response instanceof Error) {
         throw Error(`There was an unexpected error: ${response.message}`);
       }
-      displayProduct(response);
+      productArray = response.bestsellers;
+      displayProduct(productArray, i);
     }) .catch(function(error) {
       displayErrors(error.message);
     });
   });
+  $("#submit-price").on('click', function(){ // for submitting guessed price for each item
+    let userGuess = $("#price-guess").val();
+    displayPrice(userGuess);
+  });
+  $("#newProduct").on('click', function(){ // for switching out the product and hiding the results screen
+    i ++; // global variable increments every time time the function is called
+    $("#results-screen").hide();
+    displayProduct(productArray);
+  });
+
 });
